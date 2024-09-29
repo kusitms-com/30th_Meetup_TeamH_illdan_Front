259bab76d948a2f7d91f85d84771a23e537b5320
dogmania <hj296914@gmail.com>
2024-09-29 13:18:56 +0900

    :recycle: Refactor: 버전 카탈로그 수정 및 수정사항 반영

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    implementation(libs.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
