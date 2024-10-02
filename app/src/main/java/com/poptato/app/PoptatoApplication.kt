9b0ef546ceff6f4511cf51f220dd33794c4687ab
dogmania <hj296914@gmail.com>
2024-10-02 16:05:29 +0900

    :sparkles: Feat: 카카오로그인 관련 키값 설정

--- app/src/main/java/com/poptato/app/PoptatoApplication.kt
+++ app/src/main/java/com/poptato/app/PoptatoApplication.kt
@@ -1,11 +1,14 @@
 package com.poptato.app
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class PoptatoApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+
+        KakaoSdk.init(this, BuildConfig.KAKAO_APP_KEY)
     }
 }
