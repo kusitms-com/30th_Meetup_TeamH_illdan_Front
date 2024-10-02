4f7bac3e8d32ce0653405930e53a3e19cbf6506f
dogmania <hj296914@gmail.com>
2024-10-02 14:54:34 +0900

    :sparkles: Feat: Splash 구현

--- feature/splash/src/main/java/com/poptato/splash/SplashScreen.kt
+++ feature/splash/src/main/java/com/poptato/splash/SplashScreen.kt
@@ -1,4 +1,39 @@
 package com.poptato.splash
 
-class SplashScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.poptato.design_system.R
+
+@Composable
+fun SplashScreen() {
+    val viewModel: SplashViewModel = hiltViewModel()
+
+    SplashContent()
+}
+
+@Composable
+fun SplashContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_temp_splash),
+            contentDescription = "ic_splash"
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewSplash() {
+    SplashContent()
 }
