bf98fc121f835ad45b2b156f258d3459ec19dfca
dogmania <hj296914@gmail.com>
2024-10-02 16:05:53 +0900

    :sparkles: Feat: 카카오로그인 저장소 url 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
     }
 }
 
