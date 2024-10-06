ea2b74bc048427d7036f4b674ef2d476452553e2
dogmania <hj296914@gmail.com>
2024-10-06 21:42:23 +0900

    :bento: Chore: 디자인 리소스 추가

--- design-system/src/main/java/com/poptato/design_system/StringComponent.kt
+++ design-system/src/main/java/com/poptato/design_system/StringComponent.kt
@@ -1,4 +1,10 @@
 package com.poptato.design_system
 
+// Word
+const val Today = "오늘"
+const val List = "목록"
+const val History = "히스토리"
+const val Settings = "설정"
+
 // KaKaoLogin
 const val BtnKaKaoLoginText = "카카오 로그인"
