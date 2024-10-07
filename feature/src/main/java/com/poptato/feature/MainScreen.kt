802163f47e0ec3515839f24974fc7638c608939a
dogmania <hj296914@gmail.com>
2024-10-07 11:30:38 +0900

    :recycle: Refactor: 네비게이션바 아이콘과 텍스트 사이 간격 추가

--- feature/src/main/java/com/poptato/feature/MainScreen.kt
+++ feature/src/main/java/com/poptato/feature/MainScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -172,6 +173,8 @@ fun BottomNavItem(
             contentDescription = "",
             tint = Color.Unspecified
         )
+
+        Spacer(modifier = Modifier.height(4.dp))
 
         Text(
             text = type.navName,
