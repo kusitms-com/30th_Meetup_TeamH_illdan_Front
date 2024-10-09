28753d5416432ba20ca032edb3851ef333ca9455
dogmania <hj296914@gmail.com>
2024-10-09 15:23:05 +0900

    :sparkles: Feat: 백로그 Dto 구현 및 비즈니스 로직 Todo 데이터 클래스 수정

---
+++ data/src/main/java/com/poptato/data/model/response/backlog/BacklogItemResponse.kt
@@ -0,0 +1,12 @@
+package com.poptato.data.model.response.backlog
+
+import com.google.gson.annotations.SerializedName
+
+data class BacklogItemResponse(
+    @SerializedName("todoId")
+    val todoId: Long = -1,
+    @SerializedName("content")
+    val content: String = "",
+    @SerializedName("isBookmark")
+    val isBookmark: Boolean = false
+)
--- domain/src/main/java/com/poptato/domain/model/response/today/TodoItemModel.kt
+++ domain/src/main/java/com/poptato/domain/model/response/today/TodoItemModel.kt
@@ -2,7 +2,7 @@ package com.poptato.domain.model.response.today
 
 import com.poptato.domain.model.enums.TodoStatus
 
-data class TodayItemModel(
+data class TodoItemModel(
     val todoId: Long = -1,
     val content: String = "",
     val todoStatus: TodoStatus = TodoStatus.INCOMPLETE,
