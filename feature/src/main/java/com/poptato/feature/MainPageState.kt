c8951968af821e4d46518c42dea08f7bfb1ff3a9
dogmania <hj296914@gmail.com>
2024-10-09 21:23:53 +0900

    :sparkles: Feat: 바텀시트 등장 로직 구현

--- feature/src/main/java/com/poptato/feature/MainPageState.kt
+++ feature/src/main/java/com/poptato/feature/MainPageState.kt
@@ -1,8 +1,10 @@
 package com.poptato.feature
 
 import com.poptato.core.enums.BottomNavType
+import com.poptato.domain.model.response.today.TodoItemModel
 import com.poptato.ui.base.PageState
 
 data class MainPageState(
-    val bottomNavType: BottomNavType = BottomNavType.DEFAULT
+    val bottomNavType: BottomNavType = BottomNavType.DEFAULT,
+    val selectedItem: TodoItemModel = TodoItemModel()
 ): PageState
