c30597373d42b036faaf1834a77141bf3a933d16
emjayMJkim <minjeong030420@gmail.com>
2024-10-11 14:40:08 +0900

    :sparkles: FEAT: title topBar

--- design-system/src/main/java/com/poptato/design_system/StringComponent.kt
+++ design-system/src/main/java/com/poptato/design_system/StringComponent.kt
@@ -13,3 +13,6 @@ const val BtnKaKaoLoginText = "카카오 로그인"
 // Backlog
 const val BacklogTitle = "오늘 할 일은 무엇인가요?\n일단 추가해 보세요!"
 const val BacklogHint = "새로 추가하기..."
+
+// YesterdayList
+const val YesterdayListTitle = "어제 완료한 일 왼쪽으로 스와이프!"
--- feature/yesterdaylist/src/main/java/com/poptato/yesterdaylist/YesterdayListScreen.kt
+++ feature/yesterdaylist/src/main/java/com/poptato/yesterdaylist/YesterdayListScreen.kt
@@ -1,15 +1,33 @@
 package com.poptato.yesterdaylist
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.poptato.design_system.Gray00
+import com.poptato.design_system.Gray100
+import com.poptato.design_system.Gray40
+import com.poptato.design_system.R
+import com.poptato.design_system.PoptatoTypo
+import com.poptato.design_system.YesterdayListTitle
 
 @Composable
 fun YesterdayListScreen() {
@@ -26,11 +44,41 @@ fun YesterdayListScreen() {
 fun YesterdayContent(
     uiState: YesterdayListPageState = YesterdayListPageState(),
 ) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Gray100)
     ) {
-        Text(text = "테스트")
+
+        TitleTopBar()
+
+    }
+}
+
+@Composable
+fun TitleTopBar() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp)
+    ) {
+        Text(
+            text = YesterdayListTitle,
+            style = PoptatoTypo.mdMedium,
+            color = Gray00,
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+                .align(Alignment.Center)
+        )
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_close_no_bg),
+            contentDescription = "",
+            tint = Color.Unspecified,
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .size(width = 24.dp, height = 24.dp)
+        )
     }
 }
 
