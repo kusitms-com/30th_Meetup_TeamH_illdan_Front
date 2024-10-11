09bb63901cef972aab7e96d737052e0828a791ea
emjayMJkim <minjeong030420@gmail.com>
2024-10-11 15:59:47 +0900

    :sparkles: UI: 완료 버튼 배치

--- feature/yesterdaylist/src/main/java/com/poptato/yesterdaylist/YesterdayListScreen.kt
+++ feature/yesterdaylist/src/main/java/com/poptato/yesterdaylist/YesterdayListScreen.kt
@@ -4,13 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -25,9 +24,9 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.poptato.design_system.Gray00
 import com.poptato.design_system.Gray100
-import com.poptato.design_system.Gray40
-import com.poptato.design_system.R
 import com.poptato.design_system.PoptatoTypo
+import com.poptato.design_system.Primary60
+import com.poptato.design_system.R
 import com.poptato.design_system.YesterdayListTitle
 
 @Composable
@@ -49,15 +48,27 @@ fun YesterdayContent(
     uiState: YesterdayListPageState = YesterdayListPageState(),
     onClickCloseBtn: () -> Unit = {}
 ) {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Gray100)
     ) {
 
-        TitleTopBar(
-            onClickCloseBtn = onClickCloseBtn
-        )
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            TitleTopBar(
+                onClickCloseBtn = onClickCloseBtn
+            )
+
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+            )
+
+            AllCheckBtn()
+        }
 
     }
 }
@@ -88,6 +99,27 @@ fun TitleTopBar(
                 .align(Alignment.CenterEnd)
                 .size(width = 24.dp, height = 24.dp)
                 .clickable { onClickCloseBtn() }
+        )
+    }
+}
+
+@Composable
+fun AllCheckBtn() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(horizontal = 16.dp)
+            .padding(bottom = 8.dp)
+            .background(Primary60, shape = RoundedCornerShape(12.dp))
+    ) {
+        Text(
+            text = "완료",
+            style = PoptatoTypo.lgSemiBold,
+            color = Gray100,
+            modifier = Modifier
+                .padding(vertical = 15.dp)
+                .align(Alignment.Center)
         )
     }
 }
