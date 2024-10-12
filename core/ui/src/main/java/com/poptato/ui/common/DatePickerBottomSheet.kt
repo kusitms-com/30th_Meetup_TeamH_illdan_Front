b03be020d944394878f12a256e92af35b6e906a9
dogmania <hj296914@gmail.com>
2024-10-13 01:06:06 +0900

    :sparkles: Feat: 년, 월, 일 선택 바텀시트 구현

--- core/ui/src/main/java/com/poptato/ui/common/DatePickerBottomSheet.kt
+++ core/ui/src/main/java/com/poptato/ui/common/DatePickerBottomSheet.kt
@@ -2,12 +2,10 @@ package com.poptato.ui.common
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.rememberDraggableState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,110 +15,83 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.BottomSheetDefaults
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.poptato.core.enums.DateType
 import com.poptato.core.util.TimeFormatter
 import com.poptato.design_system.Complete
-import com.poptato.design_system.Confirm
+import com.poptato.design_system.Day
 import com.poptato.design_system.Gray00
 import com.poptato.design_system.Gray100
+import com.poptato.design_system.Month
 import com.poptato.design_system.PoptatoTypo
 import com.poptato.design_system.Primary60
+import com.poptato.design_system.Year
+import com.poptato.domain.model.enums.BottomSheetType
 import com.poptato.ui.util.fadingEdge
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DatePickerBottomSheet(
-    onDateSelected: (Int, Int, Int) -> Unit = { _, _, _ -> },
-    onDismissRequest: () -> Unit = {}
+    onFullDateSelected: (String) -> Unit = {},
+    onSubDateSelected: (Int, Int) -> Unit = {_, _ ->},
+    onDismissRequest: () -> Unit = {},
+    bottomSheetType: BottomSheetType = BottomSheetType.FullDate
 ) {
     val todayDate = TimeFormatter.getTodayYearMonthDay()
     val initialYear = todayDate[0].toInt()
     val initialMonth = todayDate[1].toInt()
     val initialDay = todayDate[2].toInt()
     val items = TimeFormatter.getDaysInMonth(initialYear, initialMonth)
-    val sheetState = rememberModalBottomSheetState()
-    val coroutineScope = rememberCoroutineScope()
 
     DatePickerBottomSheetContent(
         initialYear = initialYear,
         initialMonth = initialMonth,
         initialDay = initialDay,
         items  = items,
-        sheetState = sheetState,
-        scope = coroutineScope,
         onDismissRequest = onDismissRequest,
-        onDateSelected = onDateSelected
+        onFullDateSelected = onFullDateSelected,
+        onSubDateSelected = onSubDateSelected,
+        bottomSheetType = bottomSheetType
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DatePickerBottomSheetContent(
     initialYear: Int = -1,
     initialMonth: Int = -1,
     initialDay: Int = -1,
     items: List<Int> = emptyList(),
-    sheetState: SheetState = rememberModalBottomSheetState(),
-    scope: CoroutineScope = rememberCoroutineScope(),
     onDismissRequest: () -> Unit = {},
-    onDateSelected: (Int, Int, Int) -> Unit = { _, _, _ -> }
+    onFullDateSelected: (String) -> Unit = {},
+    onSubDateSelected: (Int, Int) -> Unit = {_, _ ->},
+    bottomSheetType: BottomSheetType = BottomSheetType.FullDate
 ) {
     val yearState = rememberLazyListState(initialFirstVisibleItemIndex = initialYear - 2000)
     val monthState = rememberLazyListState(initialFirstVisibleItemIndex = initialMonth - 1)
     val dayState = rememberLazyListState(initialFirstVisibleItemIndex = initialDay - 1)
 
-    ModalBottomSheet(
-        sheetState = sheetState,
-        onDismissRequest = {
-            scope.launch { sheetState.hide() }
-            onDismissRequest()
-        },
-        containerColor = Gray100,
+    Column(
         modifier = Modifier
-            .pointerInput(Unit) {
-                detectDragGestures { _, dragAmount ->
-                    if (dragAmount.y > 0) {
-                        return@detectDragGestures
-                    }
-                }
-            }
+            .fillMaxWidth()
+            .height(324.dp)
+            .background(Gray100)
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .background(Gray100)
+                .padding(top = 24.dp)
+                .padding(horizontal = 24.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Box(
@@ -130,7 +101,7 @@ fun DatePickerBottomSheetContent(
                 DialPicker(
                     items = (2000..2026).toList(),
                     listState = yearState,
-                    dateType = DateType.YEAR
+                    dateType = DateType.YEAR,
                 )
             }
 
@@ -141,19 +112,21 @@ fun DatePickerBottomSheetContent(
                 DialPicker(
                     items = (1..12).toList(),
                     listState = monthState,
-                    dateType = DateType.MONTH
+                    dateType = DateType.MONTH,
                 )
             }
 
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-            ) {
-                DialPicker(
-                    items = items,
-                    listState = dayState,
-                    dateType = DateType.DAY
-                )
+            if (bottomSheetType == BottomSheetType.FullDate) {
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                ) {
+                    DialPicker(
+                        items = items,
+                        listState = dayState,
+                        dateType = DateType.DAY,
+                    )
+                }
             }
         }
 
@@ -168,7 +141,13 @@ fun DatePickerBottomSheetContent(
                     val selectedYear = 2000 + yearState.firstVisibleItemIndex
                     val selectedMonth = 1 + monthState.firstVisibleItemIndex
                     val selectedDay = 1 + dayState.firstVisibleItemIndex
-                    onDateSelected(selectedYear, selectedMonth, selectedDay)
+
+                    if (bottomSheetType == BottomSheetType.FullDate) {
+                        onFullDateSelected("$selectedYear$Year $selectedMonth$Month $selectedDay$Day")
+                    } else if (bottomSheetType == BottomSheetType.SubDate) {
+                        onSubDateSelected(selectedYear, selectedMonth)
+                    }
+
                     onDismissRequest()
                 }
                 .clip(RoundedCornerShape(12.dp))
@@ -190,7 +169,7 @@ fun DatePickerBottomSheetContent(
 fun DialPicker(
     items: List<Int>,
     listState: LazyListState,
-    dateType: DateType = DateType.YEAR
+    dateType: DateType = DateType.YEAR,
 ) {
     val extendedItems = listOf(0, 0) + items + listOf(0, 0)
     val visibleItemsCount = 5
