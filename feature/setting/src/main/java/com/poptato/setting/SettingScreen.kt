1f95d8ed28344626fa6eb3af065888e5e91d3eff
emjayMJkim <minjeong030420@gmail.com>
2024-10-18 00:24:47 +0900

    :sparkles: Ui: 로그아웃 dialog 생성

--- feature/setting/src/main/java/com/poptato/setting/SettingScreen.kt
+++ feature/setting/src/main/java/com/poptato/setting/SettingScreen.kt
@@ -4,10 +4,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -16,18 +25,26 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.poptato.design_system.Danger50
 import com.poptato.design_system.EditProfile
 import com.poptato.design_system.Notice
 import com.poptato.design_system.FAQ
 import com.poptato.design_system.Gray00
+import com.poptato.design_system.Gray05
 import com.poptato.design_system.Gray100
 import com.poptato.design_system.Gray20
 import com.poptato.design_system.Gray60
+import com.poptato.design_system.Gray95
 import com.poptato.design_system.LogOut
+import com.poptato.design_system.LogOutDialogBackBtn
+import com.poptato.design_system.LogOutDialogDoBtn
+import com.poptato.design_system.LogOutDialogTitle
 import com.poptato.design_system.Policy
 import com.poptato.design_system.PoptatoTypo
 import com.poptato.design_system.Primary60
@@ -183,8 +200,65 @@ fun SettingServiceItem(
     }
 }
 
+@Composable
+fun MinimalDialog(
+    onDismiss: () -> Unit = {},
+    onClickLogOut: () -> Unit = {},
+    onClickBack: () -> Unit = {}
+) {
+    Dialog(onDismissRequest = onDismiss) {
+        Card(
+            modifier = Modifier
+                .wrapContentHeight()
+                .width(328.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+            backgroundColor = Gray100
+        ) {
+            Column {
+                Text(
+                    text = LogOutDialogTitle,
+                    textAlign = TextAlign.Center,
+                    color = Gray00,
+                    style = PoptatoTypo.mdSemiBold,
+                    modifier = Modifier
+                        .padding(vertical = 40.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = LogOutDialogBackBtn,
+                        textAlign = TextAlign.Center,
+                        color = Gray05,
+                        style = PoptatoTypo.mdSemiBold,
+                        modifier = Modifier
+                            .background(color = Gray95)
+                            .padding(vertical = 16.dp)
+                            .weight(1f)
+                    )
+
+                    Text(
+                        text = LogOutDialogDoBtn,
+                        textAlign = TextAlign.Center,
+                        color = Gray100,
+                        style = PoptatoTypo.mdSemiBold,
+                        modifier = Modifier
+                            .background(color = Danger50)
+                            .padding(vertical = 16.dp)
+                            .weight(1f)
+                    )
+                }
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun PreviewSetting() {
     SettingContent()
+    MinimalDialog()
 }
