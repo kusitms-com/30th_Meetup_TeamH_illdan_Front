41617457529e59e0823765a9c9e0ade529f09a84
emjayMJkim <minjeong030420@gmail.com>
2024-10-19 04:47:47 +0900

    :sparkles: Ui: 프로필 편집 screen

--- feature/setting/src/main/java/com/poptato/setting/editdata/EditUserDataScreen.kt
+++ feature/setting/src/main/java/com/poptato/setting/editdata/EditUserDataScreen.kt
@@ -1,10 +1,32 @@
 package com.poptato.setting.editdata
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.poptato.design_system.Gray00
+import com.poptato.design_system.Gray100
+import com.poptato.design_system.PoptatoTypo
+import com.poptato.design_system.ProfileDetail
+import com.poptato.design_system.R
 
 @Composable
 fun EditUserDataScreen() {
@@ -12,12 +34,75 @@ fun EditUserDataScreen() {
     val viewModel: EditUserDataViewModel = hiltViewModel()
     val uiState: EditUserDataPageState by viewModel.uiState.collectAsStateWithLifecycle()
 
-    EditUserDataContent()
+    EditUserDataContent(
+        uiState = uiState,
+        onValueChange = { newValue -> viewModel.onValueChange(newValue) }
+    )
 }
 
 @Composable
-fun EditUserDataContent() {
-    //
+fun EditUserDataContent(
+    uiState: EditUserDataPageState = EditUserDataPageState(),
+    onValueChange: (String) -> Unit = {},
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Gray100)
+    ) {
+
+        SettingTitle()
+
+        UserImg()
+    }
+}
+
+@Composable
+fun SettingTitle(
+    onClickCloseBtn: () -> Unit = {}
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp)
+    ) {
+        Text(
+            text = ProfileDetail,
+            style = PoptatoTypo.mdMedium,
+            color = Gray00,
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+                .align(Alignment.Center)
+        )
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_close_no_bg),
+            contentDescription = "",
+            tint = Color.Unspecified,
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .size(width = 24.dp, height = 24.dp)
+                .clickable { onClickCloseBtn() }
+        )
+    }
+}
+
+@Composable
+fun UserImg() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(top = 16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_person),
+            contentDescription = "img_temp_person",
+            modifier = Modifier
+                .size(80.dp)
+                .align(Alignment.Center)
+        )
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
