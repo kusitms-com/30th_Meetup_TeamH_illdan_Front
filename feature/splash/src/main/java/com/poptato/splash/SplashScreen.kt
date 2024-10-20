2d3bf4c5aa8d4218f49fe0d612b99ef1741f6590
dogmania <hj296914@gmail.com>
2024-10-20 17:10:06 +0900

    :bento: Chore: 스플래쉬, 로그인 화면 아이콘 변경

--- feature/splash/src/main/java/com/poptato/splash/SplashScreen.kt
+++ feature/splash/src/main/java/com/poptato/splash/SplashScreen.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.poptato.design_system.Gray100
 import com.poptato.design_system.R
@@ -38,8 +40,10 @@ fun SplashContent() {
         contentAlignment = Alignment.Center
     ) {
         Image(
-            painter = painterResource(id = R.drawable.ic_app),
-            contentDescription = "ic_splash"
+            painter = painterResource(id = R.drawable.ic_splash),
+            contentDescription = "ic_splash",
+            modifier = Modifier
+                .padding(bottom = 100.dp)
         )
     }
 }
