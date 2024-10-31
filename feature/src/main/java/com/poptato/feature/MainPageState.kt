ed2a8cc7dc998261a6c3513493d79c1527eeb62a
dogmania <hj296914@gmail.com>
2024-10-31 14:21:06 +0900

    :recycle: Refactor: MainScreen UI, 비즈니스 로직 분리

--- feature/src/main/java/com/poptato/feature/MainPageState.kt
+++ feature/src/main/java/com/poptato/feature/MainPageState.kt
@@ -1,10 +1,13 @@
 package com.poptato.feature
 
 import com.poptato.core.enums.BottomNavType
+import com.poptato.domain.model.enums.BottomSheetType
 import com.poptato.domain.model.response.today.TodoItemModel
 import com.poptato.ui.base.PageState
 
 data class MainPageState(
     val bottomNavType: BottomNavType = BottomNavType.DEFAULT,
-    val selectedTodoItem: TodoItemModel = TodoItemModel()
+    val selectedTodoItem: TodoItemModel = TodoItemModel(),
+    val bottomSheetType: BottomSheetType = BottomSheetType.Main,
+    val backPressedOnce: Boolean = false
 ): PageState
