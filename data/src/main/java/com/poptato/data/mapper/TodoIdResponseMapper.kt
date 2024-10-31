2161ba34f1b22c96f4e44b534902e0558f4c7193
dogmania <hj296914@gmail.com>
2024-10-31 15:07:24 +0900

    :recycle: Refactor: 중복 데이터 클래스 수정

--- data/src/main/java/com/poptato/data/mapper/TodoIdResponseMapper.kt
+++ data/src/main/java/com/poptato/data/mapper/TodoIdResponseMapper.kt
@@ -2,6 +2,7 @@ package com.poptato.data.mapper
 
 import com.poptato.data.base.Mapper
 import com.poptato.data.model.response.backlog.TodoIdResponse
+import com.poptato.domain.model.request.todo.TodoIdModel
 
 object TodoIdResponseMapper: Mapper<TodoIdResponse, TodoIdModel> {
     override fun responseToModel(response: TodoIdResponse?): TodoIdModel {
