b99cbc76af2528dbe4eaf677a215244417440c37
dogmania <hj296914@gmail.com>
2024-11-02 15:46:27 +0900

    :sparkles: Feat: 자동로그인 구현

--- feature/splash/src/main/java/com/poptato/splash/SplashViewModel.kt
+++ feature/splash/src/main/java/com/poptato/splash/SplashViewModel.kt
@@ -1,14 +1,30 @@
 package com.poptato.splash
 
+import androidx.lifecycle.viewModelScope
+import com.poptato.domain.usecase.auth.GetTokenUseCase
 import com.poptato.ui.base.BaseViewModel
+import com.poptato.ui.base.PageState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
+    private val getTokenUseCase: GetTokenUseCase
+) : BaseViewModel<SplashPageState>(SplashPageState()) {
+    init {
+        checkLogin()
+    }
 
-) : BaseViewModel<SplashPageState>(
-    SplashPageState()
-) {
-
+    private fun checkLogin() {
+        viewModelScope.launch {
+            getTokenUseCase.invoke(Unit).collect {
+                if (it.accessToken.isNotEmpty() && it.refreshToken.isNotEmpty()) {
+                    updateState(
+                        uiState.value.copy(skipLogin = true)
+                    )
+                }
+            }
+        }
+    }
 }
