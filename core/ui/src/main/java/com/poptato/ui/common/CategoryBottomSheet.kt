f93d2fd04ff38ec9d4cc3c8d8b41f08631a43305
emjayMJkim <minjeong030420@gmail.com>
2024-11-15 02:09:20 +0900

    :sparkles: Feat: category 바텀시트 UI 구현

--- core/ui/src/main/java/com/poptato/ui/common/CategoryBottomSheet.kt
+++ core/ui/src/main/java/com/poptato/ui/common/CategoryBottomSheet.kt
@@ -3,31 +3,43 @@ package com.poptato.ui.common
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.poptato.design_system.Gray60
 import com.poptato.design_system.Gray80
 import com.poptato.design_system.Gray90
+import com.poptato.design_system.PoptatoTypo
 import com.poptato.domain.model.response.category.CategoryIconTotalListModel
+import com.poptato.domain.model.response.category.CategoryIconTypeListModel
 
 @Composable
 fun CategoryBottomSheet(
     categoryIconList: CategoryIconTotalListModel = CategoryIconTotalListModel()
 ) {
 
-    CategoryBottomSheetContent()
+    CategoryBottomSheetContent(categoryIconList)
 }
 
 @Composable
-fun CategoryBottomSheetContent() {
+fun CategoryBottomSheetContent(
+    categoryIconList: CategoryIconTotalListModel = CategoryIconTotalListModel()
+) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -36,13 +48,62 @@ fun CategoryBottomSheetContent() {
     ) {
         Box(
             modifier = Modifier
-                .padding(12.dp)
+                .padding(top = 12.dp, bottom = 16.dp)
                 .height(4.dp)
                 .width(64.dp)
                 .clip(RoundedCornerShape(4.dp))
                 .background(Gray80)
                 .align(Alignment.CenterHorizontally)
-        ) {}
+        )
+
+        CategoryIconListTotal(
+            categoryIconList = categoryIconList
+        )
+    }
+}
+
+@Composable
+fun CategoryIconListTotal(
+    categoryIconList: CategoryIconTotalListModel = CategoryIconTotalListModel(),
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        items(categoryIconList.icons, key = { it.iconType }) { iconTypes ->
+            CategoryIconListForType(iconTypes)
+
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+    }
+}
+
+@Composable
+fun CategoryIconListForType(
+    categoryIcons: CategoryIconTypeListModel = CategoryIconTypeListModel()
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(horizontal = 24.dp)
+    ) {
+        Text(
+            text = categoryIcons.iconType,
+            style = PoptatoTypo.smMedium,
+            color = Gray60
+        )
+
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(top = 8.dp)
+        ) {
+            items(categoryIcons.icons, key = { it.iconId }) { item ->
+                //
+            }
+        }
     }
 }
 
