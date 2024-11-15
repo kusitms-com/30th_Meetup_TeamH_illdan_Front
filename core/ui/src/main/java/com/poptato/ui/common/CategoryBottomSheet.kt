d006fe078acc8993c294d625fdd458d4ec90c84c
emjayMJkim <minjeong030420@gmail.com>
2024-11-16 01:59:54 +0900

    :sparkles: Feat: category 아이콘 리스트 UI 구현

--- core/ui/src/main/java/com/poptato/ui/common/CategoryBottomSheet.kt
+++ core/ui/src/main/java/com/poptato/ui/common/CategoryBottomSheet.kt
@@ -1,17 +1,21 @@
 package com.poptato.ui.common
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
@@ -19,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.poptato.design_system.Gray60
@@ -78,6 +83,7 @@ fun CategoryIconListTotal(
     }
 }
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun CategoryIconListForType(
     categoryIcons: CategoryIconTypeListModel = CategoryIconTypeListModel()
@@ -94,14 +100,21 @@ fun CategoryIconListForType(
             color = Gray60
         )
 
-        LazyRow(
+        FlowRow(
+            maxItemsInEachRow = 7,
             modifier = Modifier
                 .fillMaxWidth()
-                .wrapContentHeight()
-                .padding(top = 8.dp)
+                .padding(top = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
         ) {
-            items(categoryIcons.icons, key = { it.iconId }) { item ->
-                //
+
+            categoryIcons.icons.forEach { item ->
+                Image(painter = painterResource(id = com.poptato.design_system.R.drawable.ic_history_calendar),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(32.dp)
+                )
             }
         }
     }
