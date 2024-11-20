9dd685c7f663dca7c23a08be4e2305a2c5d0c1ac
emjayMJkim <minjeong030420@gmail.com>
2024-11-21 05:05:13 +0900

    :recycle: Refactor: 어제 한일 조회 API 수정

--- data/src/main/java/com/poptato/data/model/response/yesterday/YesterdayListItemResponse.kt
+++ data/src/main/java/com/poptato/data/model/response/yesterday/YesterdayListItemResponse.kt
@@ -6,5 +6,11 @@ data class YesterdayListItemResponse (
     @SerializedName("todoId")
     val todoId: Long = -1,
     @SerializedName("content")
-    val content: String = ""
+    val content: String = "",
+    @SerializedName("dday")
+    val dday: Int,
+    @SerializedName("bookmark")
+    val bookmark: Boolean,
+    @SerializedName("repeat")
+    val repeat: Boolean
 )
