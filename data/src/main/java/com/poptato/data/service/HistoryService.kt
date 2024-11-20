759e4724c3418934fa2522991a882f17f7a5805a
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-20 18:32:17 +0900

    ✨ Feat: Service 추가

--- data/src/main/java/com/poptato/data/service/HistoryService.kt
+++ data/src/main/java/com/poptato/data/service/HistoryService.kt
@@ -2,6 +2,7 @@ package com.poptato.data.service
 
 import com.poptato.data.base.ApiResponse
 import com.poptato.data.base.Endpoints
+import com.poptato.data.model.response.history.HistoryCalendarListResponse
 import com.poptato.data.model.response.history.HistoryListResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -15,4 +16,10 @@ interface HistoryService {
         @Query("size") size: Int,
         @Query("date") date: String,
     ): Response<ApiResponse<HistoryListResponse>>
+
+    @GET(Endpoints.History.CALENDAR)
+    suspend fun getHistoryCalendarList(
+        @Query("year") year: String,
+        @Query("month") month: Int
+    ): Response<ApiResponse<HistoryCalendarListResponse>>
 }
