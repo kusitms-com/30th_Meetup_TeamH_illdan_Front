1057bd779365808d2b4602c088fd2f07ee40da7d
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-20 18:32:24 +0900

    ✨ Feat: repositoryImpl 추가

--- data/src/main/java/com/poptato/data/repository/HistoryRepositoryImpl.kt
+++ data/src/main/java/com/poptato/data/repository/HistoryRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.poptato.data.repository
 
 import com.poptato.data.base.BaseRepository
+import com.poptato.data.mapper.HistoryCalendarResponseMapper
 import com.poptato.data.mapper.HistoryResponseMapper
 import com.poptato.data.service.HistoryService
+import com.poptato.domain.model.response.history.HistoryCalendarListModel
 import com.poptato.domain.model.response.history.HistoryListModel
 import com.poptato.domain.repository.HistoryRepository
 import kotlinx.coroutines.flow.Flow
@@ -13,5 +15,12 @@ class HistoryRepositoryImpl @Inject constructor(
 ): HistoryRepository, BaseRepository() {
     override suspend fun getHistoryList(page: Int, size: Int, date: String): Flow<Result<HistoryListModel>> {
         return apiLaunch(apiCall = { historyService.getHistoryList(page, size, date) }, HistoryResponseMapper)
+    }
+
+    override suspend fun getHistoryCalendarList(
+        year: String,
+        month: Int
+    ): Flow<Result<HistoryCalendarListModel>> {
+        return apiLaunch(apiCall = { historyService.getHistoryCalendarList(year, month) }, HistoryCalendarResponseMapper)
     }
 }
